1885344cbecc10516a1bd7037d1142c47bd98649
Aryam Martinez <aryamalejand15@gmail.com>
2020-12-15 14:15:00 -0300

    version 1

---
+++ js/index.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.logo', {
+        duration: 1000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    
+    sr.reveal('.scroll-banner', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+
+    
+    sr.reveal('.scroll_music', {
+        duration: 2000,
+        origin:'left',
+        distance: '400px'
+    });
+
+    sr.reveal('#contenedor-repro', {
+        duration: 2000,
+        origin:'right',
+        distance: '400px'
+    });
+
+    
